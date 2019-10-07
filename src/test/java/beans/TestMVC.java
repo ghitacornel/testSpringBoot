cc1981b0000f1956a24d38d09db73760e0dcaee7
ghitacornel <ghita.cornel@gmail.com>
2019-10-07 16:41:07 +0300

    xxx

--- src/test/java/beans/TestMVC.java
+++ src/test/java/beans/TestMVC.java
@@ -5,11 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import template.AbstractTestSpringBootContext;
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import template.Utils;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -22,7 +18,7 @@ public class TestMVC extends AbstractTestSpringBootContext {
 
     @Test
     public void testController() throws Exception {
-        String content = readFile("testMVC.html");
+        String content = Utils.readFile("testMVC.html");
         mvc.perform(get("/custom?name=ion")
                 .contentType(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
@@ -30,14 +26,4 @@ public class TestMVC extends AbstractTestSpringBootContext {
                 .andExpect(content().string(content));
     }
 
-    static String readFile(String fileName) {
-        try {
-            Path resourceDirectory = Paths.get("src", "test", "resources");
-            String absolutePath = resourceDirectory.toFile().getAbsolutePath();
-            byte[] encoded = Files.readAllBytes(Paths.get(absolutePath, fileName));
-            return new String(encoded, StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
--- src/test/java/beans/TestREST.java
+++ src/test/java/beans/TestREST.java
@@ -5,11 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import template.AbstractTestSpringBootContext;
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import template.Utils;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -22,7 +18,7 @@ public class TestREST extends AbstractTestSpringBootContext {
 
     @Test
     public void testRestController() throws Exception {
-        String content = readFile("testREST.json");
+        String content = Utils.readFile("testREST.json");
         mvc.perform(get("/users/")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -30,14 +26,4 @@ public class TestREST extends AbstractTestSpringBootContext {
                 .andExpect(content().string(content));
     }
 
-    static String readFile(String fileName) {
-        try {
-            Path resourceDirectory = Paths.get("src", "test", "resources");
-            String absolutePath = resourceDirectory.toFile().getAbsolutePath();
-            byte[] encoded = Files.readAllBytes(Paths.get(absolutePath, fileName));
-            return new String(encoded, StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
