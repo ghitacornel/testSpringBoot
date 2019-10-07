7db7ab5fdbf8f680bcfd465fe753b0ea5577f9f0
ghitacornel <ghita.cornel@gmail.com>
2019-10-07 15:13:13 +0300

    initial

--- src/test/java/template/AbstractTestSpringBootContext.java
+++ src/test/java/template/AbstractTestSpringBootContext.java
@@ -1,14 +1,13 @@
 package template;
 
-import beans.SpringBootMain;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {SpringBootMain.class})
+@SpringBootTest
 public abstract class AbstractTestSpringBootContext {
 
     @Autowired
