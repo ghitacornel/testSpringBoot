c407baf171e2a25251e8ccb0d8289414fa52265e
ghitacornel <ghita.cornel@gmail.com>
2019-10-18 11:25:29 +0300

    xxx

--- src/main/java/beans/servlets/CustomServlet.java
+++ src/main/java/beans/servlets/CustomServlet.java
@@ -39,4 +39,16 @@ public class CustomServlet extends HttpServlet {
 
     }
 
+    @Override
+    protected void service(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<p>Hello World! " + request.getMethod() + " custom servlet</p>");
+
+    }
+
 }
