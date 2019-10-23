46cef489f8e02738dea22670dd513d6d084fbf6d
ghitacornel <ghita.cornel@gmail.com>
2019-10-23 10:58:00 +0300

    xxx

--- src/main/java/beans/services/BusinessService.java
+++ src/main/java/beans/services/BusinessService.java
@@ -3,20 +3,13 @@ package beans.services;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
 public class BusinessService {
 
     @Value("${my.custom.setting}")
     private String setting;
 
-    @PostConstruct
-    public void init(){
-        System.err.println(setting);
-    }
-
     public String calculateName(String input) {
-        return input + input;
+        return input + " " + setting;
     }
 }
