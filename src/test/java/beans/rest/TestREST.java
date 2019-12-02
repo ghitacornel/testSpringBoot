9317d0ab81541638973f800cb4018f527bc69848
cornel <ghita_cornel@yahoo.com>
2019-12-02 23:40:04 +0200

    xxx

--- src/test/java/beans/rest/TestREST.java
+++ src/test/java/beans/rest/TestREST.java
@@ -45,7 +45,7 @@ public class TestREST extends AbstractTestSpringBootContext {
                 .param("id", "1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(content().json("{\"id\":1,\"name\":\"ion\",\"password\":\"db pass ion\"}"));
+                .andExpect(content().json("{id:1,name:\"ion\",password:\"db pass ion\"}"));
     }
 
     @Test
@@ -72,7 +72,7 @@ public class TestREST extends AbstractTestSpringBootContext {
         mvc.perform(get("/user")
                 .param("id", "3"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"id\":3,\"name\":\"vasile\",\"password\":\"db pass vasile\"}"));
+                .andExpect(content().json("{id:3,name:\"vasile\",password:\"db pass vasile\"}"));
 
         // update
         mvc.perform(post("/user")
@@ -85,7 +85,7 @@ public class TestREST extends AbstractTestSpringBootContext {
         mvc.perform(get("/user")
                 .param("id", "3"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"id\":3,\"name\":\"vasile 1\",\"password\":\"db pass vasile 1\"}"));
+                .andExpect(content().json("{id:3,name:\"vasile 1\",password:\"db pass vasile 1\"}"));
 
         // delete
         mvc.perform(delete("/user/{id}", "3")).andExpect(status().isOk());
