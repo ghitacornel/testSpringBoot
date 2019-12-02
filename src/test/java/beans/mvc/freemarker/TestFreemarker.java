3e66e90ce7ccc9ed782e5da40a53b1af2bcabca7
cornel <ghita_cornel@yahoo.com>
2019-12-02 23:50:33 +0200

    xxx

--- src/test/java/beans/mvc/freemarker/TestFreemarker.java
+++ src/test/java/beans/mvc/freemarker/TestFreemarker.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class TestMVC extends AbstractTestSpringBootContext {
+public class TestFreemarker extends AbstractTestSpringBootContext {
 
     @Autowired
     MockMvc mvc;
