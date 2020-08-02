79f7210368aef081cd851befd5b81aa401310a82
ghitacornel <ghita_cornel@yahoo.com>
2020-08-02 13:56:52 +0300

    push version

--- src/main/java/beans/SpringBootMain.java
+++ src/main/java/beans/SpringBootMain.java
@@ -6,12 +6,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * MAIN class<br>
- * Also it scans for classes considering the current package as the root scan package
+ * Spring scans for classes considering the current package as the root scan package
  */
 @SpringBootApplication
 @EnableJpaRepositories("beans.rest.jpa.repository")
 public class SpringBootMain {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMain.class, args);
     }
