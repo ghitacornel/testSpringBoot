4d3ce1189a377b44169d3ccba9e4b644d3aae278
cornel <ghita.cornel@gmail.com>
2021-03-26 01:29:14 +0200

    bt

--- src/test/java/template/AbstractTestSpringBootContext.java
+++ src/test/java/template/AbstractTestSpringBootContext.java
@@ -6,17 +6,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public abstract class AbstractTestSpringBootContext {
-
-    @Autowired
-    protected ConfigurableApplicationContext context;
 
     @Autowired
     private Flyway flyway;
