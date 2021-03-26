92dbf3e913d07683d3f8eb989fa8fc8d7584c3e6
cornel <ghita.cornel@gmail.com>
2021-03-26 02:09:55 +0200

    bt

--- src/main/java/beans/rest/jpa/repository/ChildRepository.java
+++ src/main/java/beans/rest/jpa/repository/ChildRepository.java
@@ -1,11 +1,7 @@
 package beans.rest.jpa.repository;
 
 import beans.rest.jpa.model.Child;
-import beans.rest.jpa.model.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ChildRepository extends JpaRepository<Child, Integer> {
-
 }
--- src/main/java/beans/rest/jpa/repository/ParentRepository.java
+++ src/main/java/beans/rest/jpa/repository/ParentRepository.java
@@ -1,11 +1,7 @@
 package beans.rest.jpa.repository;
 
 import beans.rest.jpa.model.Parent;
-import beans.rest.jpa.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ParentRepository extends JpaRepository<Parent, Integer> {
-
 }
