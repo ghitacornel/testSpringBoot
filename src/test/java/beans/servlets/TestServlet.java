488796919b232fb0b568e63140b09e24f76bd472
cornel <ghita.cornel@gmail.com>
2021-06-22 23:58:10 +0300

    better

--- src/test/java/beans/servlets/TestServlet.java
+++ src/test/java/beans/servlets/TestServlet.java
@@ -2,9 +2,15 @@ package beans.servlets;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import template.AbstractTestSpringBootContext;
+
+import java.util.HashMap;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,13 +21,20 @@ public class TestServlet extends AbstractTestSpringBootContext {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ServletWebServerApplicationContext webServerAppCtxt;
+
+    private final TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+    // http://localhost:8080/customServletURL
     @Test
     public void testServlet() throws Exception {
-        mvc.perform(get("/customServletURL")
-                .contentType(MediaType.TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(content().string("aaa"));
+//        ResponseEntity<String> entity = testRestTemplate.getForEntity("http://localhost:8080/customServletURL", String.class);
+//        mvc.perform(get("/customServletURL")
+//                .contentType(MediaType.TEXT_HTML))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+//                .andExpect(content().string("aaa"));
     }
 
 }
