cb265ce4ac0d7a512641c3649092a31486f632ac
cornel <ghita.cornel@gmail.com>
2021-09-23 13:36:00 +0300

    better

--- src/main/java/beans/rest/jpa/repository/ChildRepository.java
+++ src/main/java/beans/rest/jpa/repository/ChildRepository.java
@@ -2,6 +2,13 @@ package beans.rest.jpa.repository;
 
 import beans.rest.jpa.model.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
 
 public interface ChildRepository extends JpaRepository<Child, Integer> {
+
+    @Query("select distinct t.name from Child t")
+    Set<String> findOnlyNames();
+
 }
