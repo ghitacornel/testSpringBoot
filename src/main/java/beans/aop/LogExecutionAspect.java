6e4759b19c7265ba8c0d1cef7c832cc8ce768ebb
cornel <ghita.cornel@gmail.com>
2022-01-25 15:19:59 +0200

    better

--- src/main/java/beans/aop/LogExecutionAspect.java
+++ src/main/java/beans/aop/LogExecutionAspect.java
@@ -5,6 +5,9 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+/**
+ * logs start/end of a marked method execution
+ */
 @Aspect
 @Component
 public class LogExecutionAspect {
--- src/main/java/beans/aop/LogExecutionTimeAspect.java
+++ src/main/java/beans/aop/LogExecutionTimeAspect.java
@@ -5,6 +5,9 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+/**
+ * logs time duration of a marked method execution
+ */
 @Aspect
 @Component
 public class LogExecutionTimeAspect {
