d6750a31bfaafc6de79c121eb80e591b74b8d7b1
cornel <ghita.cornel@gmail.com>
2022-01-31 14:40:03 +0200

    better

--- src/main/java/beans/rest/exceptions/ControllerExceptionHandler.java
+++ src/main/java/beans/rest/exceptions/ControllerExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
