7aabcb151697c12f6ec434304c04b748adec7ba2
cornel <ghita.cornel@gmail.com>
2022-02-25 11:56:32 +0200

    better

--- src/main/java/beans/rest/retry/exceptions/ResourceException.java
+++ src/main/java/beans/rest/retry/exceptions/ResourceException.java
@@ -2,6 +2,9 @@ package beans.rest.retry.exceptions;
 
 import lombok.experimental.StandardException;
 
+/**
+ * Integration with systems that might fail must be controlled through the usage of dedicated exceptions
+ */
 @StandardException
 public class ResourceException extends RuntimeException {
     public ResourceException(String message) {
