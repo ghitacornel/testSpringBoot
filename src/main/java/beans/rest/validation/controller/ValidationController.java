4cee9c68b75f95846d47894eda8a5cf77bb32b8e
cornel <ghita.cornel@gmail.com>
2022-03-09 23:30:34 +0200

    better

--- src/main/java/beans/rest/validation/controller/ValidationController.java
+++ src/main/java/beans/rest/validation/controller/ValidationController.java
@@ -2,6 +2,12 @@ package beans.rest.validation.controller;
 
 import beans.rest.validation.model.Model;
 import beans.rest.validation.service.ValidationService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@OpenAPIDefinition(info = @Info(title = "Validation", description = "Controller used for validation", version = "1"))
 @RestController
 @RequestMapping(value = "validate")
 @RequiredArgsConstructor
@@ -17,15 +24,25 @@ public class ValidationController {
 
     private final ValidationService service;
 
-    // validation performed in REST layer
+    @Operation(summary = "REST layer validation", description = "validation performed automatically in REST layer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "validation succeeded"),
+            @ApiResponse(responseCode = "400", description = "Bad request, validation failed")
+    }
+    )
     @PutMapping(value = "rest")
-    public Model invokeRest(@Valid @RequestBody Model model) {
+    public Model invokeRest(@Parameter(description = "Model to validate in REST") @Valid @RequestBody Model model) {
         return model;
     }
 
-    // validation performed in Service layer
+    @Operation(summary = "Service layer validation", description = "validation performed automatically in Service layer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "validation succeeded"),
+            @ApiResponse(responseCode = "400", description = "Bad request, validation failed")
+    }
+    )
     @PutMapping(value = "service")
-    public Model invokeService(@RequestBody Model model) {
+    public Model invokeService(@Parameter(description = "Model to validate in Service", required = true) @RequestBody Model model) {
         return service.invoke(model);
     }
 
