8037acfab915ea6442ccb8208b36b2c124423430
cornel <ghita.cornel@gmail.com>
2022-03-10 00:33:02 +0200

    better

--- src/test/java/beans/rest/validate/TestRestLayerValidation.java
+++ src/test/java/beans/rest/validate/TestRestLayerValidation.java
@@ -23,7 +23,7 @@ public class TestRestLayerValidation extends AbstractTestSpringBootContext {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"id\":3,\"name\":\"vasile\"}"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"id\":3,\"name\":\"vasile\"}"));
+                .andExpect(content().string("{\"id\":3,\"name\":\"vasile\"}"));
     }
 
     @Test
@@ -32,7 +32,16 @@ public class TestRestLayerValidation extends AbstractTestSpringBootContext {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"vasile\"}"))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().json("[{\"fieldName\":\"id\",\"message\":\"must not be null\",\"messageCode\":\"NotNull\"}]"));
+                .andExpect(content().string("[{\"fieldName\":\"id\",\"message\":\"must not be null\",\"messageCode\":\"NotNull\"}]"));
+    }
+
+    @Test
+    public void testIdIsNegative() throws Exception {
+        mvc.perform(put(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":-3,\"name\":\"vasile\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("[{\"fieldName\":\"id\",\"message\":\"must be greater than 0\",\"messageCode\":\"Positive\"}]"));
     }
 
     @Test
@@ -41,7 +50,7 @@ public class TestRestLayerValidation extends AbstractTestSpringBootContext {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"id\":3}"))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().json("[{\"fieldName\":\"name\",\"message\":\"must not be empty\",\"messageCode\":\"NotEmpty\"}]"));
+                .andExpect(content().string("[{\"fieldName\":\"name\",\"message\":\"must not be empty\",\"messageCode\":\"NotEmpty\"}]"));
     }
 
     @Test
