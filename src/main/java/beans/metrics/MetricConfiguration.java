956a86bbc4cf21218b45c9d624c95134b4fc9cb7
cornel <ghita.cornel@gmail.com>
2022-03-10 23:01:05 +0200

    better

--- src/main/java/beans/metrics/MetricConfiguration.java
+++ src/main/java/beans/metrics/MetricConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MetricConfig {
+public class MetricConfiguration {
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
