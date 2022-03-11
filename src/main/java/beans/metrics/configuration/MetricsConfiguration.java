835c9d362be0934799a6d623a746c432a78beba7
cornel <ghita.cornel@gmail.com>
2022-03-11 15:09:14 +0200

    better

--- src/main/java/beans/metrics/configuration/MetricsConfiguration.java
+++ src/main/java/beans/metrics/configuration/MetricsConfiguration.java
@@ -3,17 +3,11 @@ package beans.metrics.configuration;
 import io.micrometer.core.aop.CountedAspect;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsConfiguration {
-
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        return registry -> registry.config().commonTags("application", "monitored");
-    }
 
     @Bean
     TimedAspect timedAspect(MeterRegistry registry) {
