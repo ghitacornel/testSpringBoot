2a5cc5ad900bbd72a1563b8da08abfc8cae8f912
cornel <ghita.cornel@gmail.com>
2022-03-11 08:12:08 +0200

    better

--- src/main/java/beans/rest/versioning/HeaderVersioningController.java
+++ src/main/java/beans/rest/versioning/HeaderVersioningController.java
@@ -3,6 +3,7 @@ package beans.rest.versioning;
 import beans.rest.versioning.model.Student;
 import beans.rest.versioning.model.StudentV1;
 import beans.rest.versioning.model.StudentV2;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,19 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Controller with REST endpoint versioning using HTTP headers
  */
+@Hidden// due to issues on OpenAPI parsing of headers
 @RestController
 @RequestMapping(value = "version/header")
 public class HeaderVersioningController {
 
-//    @GetMapping(value = "student", headers = "X-API-VERSION=1")
-//    public StudentV1 studentV1() {
-//        return new StudentV1("Bob Charlie");
-//    }
-//
-//    @GetMapping(value = "student", headers = "X-API-VERSION=2")
-//    public StudentV2 studentV2() {
-//        return new StudentV2("Bob", "Charlie");
-//    }
+    @GetMapping(value = "student", headers = "X-API-VERSION=1")
+    public StudentV1 studentV1() {
+        return new StudentV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "student", headers = "X-API-VERSION=2")
+    public StudentV2 studentV2() {
+        return new StudentV2("Bob", "Charlie");
+    }
 
     /**
      * no header => default use latest
