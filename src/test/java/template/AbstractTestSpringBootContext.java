61efa5a26e8808f3e8af6d8d9b67fe5f4752b677
cornel <ghita.cornel@gmail.com>
2022-03-15 23:39:15 +0200

    better

--- src/test/java/template/AbstractTestSpringBootContext.java
+++ src/test/java/template/AbstractTestSpringBootContext.java
@@ -1,14 +1,9 @@
 package template;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public abstract class AbstractTestSpringBootContext {
-
-
 }
