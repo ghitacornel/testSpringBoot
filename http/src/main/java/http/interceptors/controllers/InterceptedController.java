86296edc69245997d226acbd65f5db7ba6c8712a
cornel <ghita.cornel@gmail.com>
2022-04-07 15:14:46 +0300

    better

--- http/src/main/java/http/interceptors/controllers/InterceptedController.java
+++ http/src/main/java/http/interceptors/controllers/InterceptedController.java
@@ -1,6 +1,5 @@
 package http.interceptors.controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
