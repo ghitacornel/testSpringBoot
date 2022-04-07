1a08e8ba2cdfc8e7f97e5ff285790f98f7735cf9
cornel <ghita.cornel@gmail.com>
2022-04-07 13:18:41 +0300

    better

--- exceptions-validations/src/main/java/beans/exceptions/handler/CustomBusinessControllerExceptionHandler.java
+++ exceptions-validations/src/main/java/beans/exceptions/handler/CustomBusinessControllerExceptionHandler.java
@@ -1,14 +1,12 @@
 package beans.exceptions.handler;
 
 import beans.exceptions.exception.CustomBusinessException;
-import beans.exceptions.exception.CustomBusinessExceptionMarked;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,9 +18,9 @@ public class CustomBusinessControllerExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), BAD_REQUEST);
     }
 
-    @ExceptionHandler(CustomBusinessExceptionMarked.class)
-    public ResponseEntity<Object> handleCustomBusinessExceptionMarked(CustomBusinessExceptionMarked e) {
-        return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
-    }
+//    @ExceptionHandler(CustomBusinessExceptionMarked.class)
+//    public ResponseEntity<Object> handleCustomBusinessExceptionMarked(CustomBusinessExceptionMarked e) {
+//        return new ResponseEntity<>(e.getMessage(), NOT_FOUND);// NOT NEEDED, automatically processed
+//    }
 
 }
