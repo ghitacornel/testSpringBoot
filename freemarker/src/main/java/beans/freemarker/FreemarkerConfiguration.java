3d8b60386921b55352229f9e543af1db8fbbafc8
cornel <ghita.cornel@gmail.com>
2022-04-07 09:50:12 +0300

    better

---
+++ freemarker/src/main/java/beans/freemarker/FreemarkerConfiguration.java
@@ -0,0 +1,27 @@
+package beans.freemarker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+public class FreemarkerConfiguration {
+
+    @Bean
+    public FreeMarkerConfigurer freemarkerConfig() {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPath("classpath:freemarker");
+        return freeMarkerConfigurer;
+    }
+
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        resolver.setContentType("text/html;charset=UTF-8");
+        return resolver;
+    }
+}
