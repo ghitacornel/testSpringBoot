28091216ae5ce6f07f4f28f2913ceb0e8441d341
cornel <ghita.cornel@gmail.com>
2022-04-13 10:06:31 +0300

    better

---
+++ rest-clients/src/main/java/beans/clients/webflux/ExternalRestClientWebFlux.java
@@ -0,0 +1,40 @@
+package beans.clients.webflux;
+
+import beans.external.ExternalRestServiceInputModel;
+import beans.external.ExternalRestServiceOutputModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class ExternalRestClientWebFlux {
+    private final ServerProperties serverProperties;
+
+    public String callExternalService(String input) {
+        ExternalRestServiceInputModel inputModel = new ExternalRestServiceInputModel();
+        inputModel.setInput(input);
+
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:" + serverProperties.getPort())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        ExternalRestServiceOutputModel outputModel = webClient
+                .post()
+                .uri("externalService")
+                .bodyValue(inputModel)
+                .retrieve()
+                .bodyToMono(ExternalRestServiceOutputModel.class)
+                .block();
+
+        return outputModel.getOutput() + " + added by internal client";
+    }
+}
---
+++ rest-clients/src/test/java/beans/clients/webclient/ExternalRestClientWebClientTest.java
@@ -0,0 +1,19 @@
+package beans.clients.webclient;
+
+import beans.clients.AbstractTestSpringBootContext;
+import beans.clients.resttemplate.ExternalRestClientRestTemplate;
+import beans.clients.webflux.ExternalRestClientWebFlux;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ExternalRestClientWebClientTest extends AbstractTestSpringBootContext {
+
+    @Autowired
+    ExternalRestClientWebFlux client;
+
+    @Test
+    public void testClient() {
+        Assertions.assertThat(client.callExternalService("input data")).isEqualTo("input data + added by external client + added by internal client");
+    }
+}
