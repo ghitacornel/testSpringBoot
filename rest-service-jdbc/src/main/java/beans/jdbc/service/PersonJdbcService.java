dafa9535eb8c0ec0233906b73b002fe4ff343fd3
cornel <ghita.cornel@gmail.com>
2022-09-11 21:55:32 +0300

    better

--- rest-service-jdbc/src/main/java/beans/jdbc/service/PersonJdbcService.java
+++ rest-service-jdbc/src/main/java/beans/jdbc/service/PersonJdbcService.java
@@ -2,18 +2,16 @@ package beans.jdbc.service;
 
 import beans.jdbc.model.PersonJdbc;
 import beans.jdbc.repository.PersonJdbcRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PersonJdbcService {
 
     final private PersonJdbcRepository repository;
-
-    public PersonJdbcService(PersonJdbcRepository repository) {
-        this.repository = repository;
-    }
 
     public List<PersonJdbc> findAll() {
         return repository.findAll();
