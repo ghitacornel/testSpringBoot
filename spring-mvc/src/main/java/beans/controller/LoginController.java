80a79eab505092e6da91402bdb395741f89f5f5e
cornel <ghita.cornel@gmail.com>
2022-09-11 15:36:42 +0300

    better

--- spring-mvc/src/main/java/beans/controller/LoginController.java
+++ spring-mvc/src/main/java/beans/controller/LoginController.java
@@ -1,15 +1,13 @@
 package beans.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String gotoLoginPage(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+    public String gotoLoginPage() {
         return "loginPage";
     }
 }
