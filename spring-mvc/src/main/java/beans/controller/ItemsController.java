878b577d1bfaa66ddaa03b07de9a562ba3c65763
cornel <ghita.cornel@gmail.com>
2022-09-11 15:01:21 +0300

    better

--- spring-mvc/src/main/java/beans/controller/ItemsController.java
+++ spring-mvc/src/main/java/beans/controller/ItemsController.java
@@ -1,13 +1,16 @@
 package beans.controller;
 
+import beans.controller.model.DeleteRequest;
 import beans.controller.model.LoginData;
 import beans.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -22,6 +25,13 @@ public class ItemsController {
         // TODO add security check
         log.error("user logged with credentials : user = " + loginData.getUser() + " ; pass = " + loginData.getPass());
 
+        model.addAttribute("items", service.findAll());
+        return "itemsPage";
+    }
+
+    @PostMapping("/items/delete")
+    public String delete(@ModelAttribute(value="deleteRequest") DeleteRequest request, Model model) {
+        service.deleteById(request.getId());
         model.addAttribute("items", service.findAll());
         return "itemsPage";
     }
---
+++ spring-mvc/src/main/java/beans/controller/model/DeleteRequest.java
@@ -0,0 +1,8 @@
+package beans.controller.model;
+
+import lombok.Data;
+
+@Data
+public class DeleteRequest {
+    private Integer id;
+}
