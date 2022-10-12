8fb55ac2d41a1d481fd5913c941c12dad038aba6
cornel <ghita.cornel@gmail.com>
2022-10-12 22:31:03 +0300

    better

--- elasticsearch/src/main/java/beans/model/Parent.java
+++ elasticsearch/src/main/java/beans/model/Parent.java
@@ -2,6 +2,9 @@ package beans.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.search.annotations.Analyzer;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -13,12 +16,17 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@Indexed(index = "idx_parent")
 public class Parent {
 
     @Id
     private Integer id;
 
+    @Field
     private String name;
+
+    @Field(name = "content", analyzer = @Analyzer(definition = "stop"))
+    private String content;
 
     @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     final private List<Child> children = new ArrayList<>();
