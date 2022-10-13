993c86983bb1f57cabe3711495ddcb84388bbbc9
cornel <ghita.cornel@gmail.com>
2022-10-13 15:17:49 +0300

    better

--- elasticsearch/src/main/java/beans/service/ElasticSearchService.java
+++ elasticsearch/src/main/java/beans/service/ElasticSearchService.java
@@ -1,14 +1,34 @@
 package beans.service;
 
 import beans.model.Parent;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.search.engine.search.predicate.dsl.BooleanPredicateClausesStep;
+import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.scope.SearchScope;
+import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class ElasticSearchService {
+
+    private final EntityManager entityManager;
+
     public List<Parent> findByContent(String content) {
-        return new ArrayList<>();
+        SearchSession searchSession = Search.session(entityManager);
+        SearchScope<Parent> scope = searchSession.scope(Parent.class);
+        SearchPredicateFactory predicateFactory = scope.predicate();
+        BooleanPredicateClausesStep<?> booleanJunction = predicateFactory.bool();
+
+        booleanJunction.must(predicateFactory.match().field("content").matching(content).toPredicate());
+        booleanJunction.must(predicateFactory.match().field("name").matching(content).toPredicate());
+
+        return searchSession.search(Parent.class).where(booleanJunction.toPredicate()).fetchAllHits();
     }
 }
