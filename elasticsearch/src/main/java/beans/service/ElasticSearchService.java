2c1cfc046d681e324f05922650859db9ae0dfd5e
cornel <ghita.cornel@gmail.com>
2022-10-13 14:40:42 +0300

    better

--- elasticsearch/src/main/java/beans/service/ElasticSearchService.java
+++ elasticsearch/src/main/java/beans/service/ElasticSearchService.java
@@ -3,11 +3,12 @@ package beans.service;
 import beans.model.Parent;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ElasticSearchService {
     public List<Parent> findByContent(String content) {
-        return null;
+        return new ArrayList<>();
     }
 }
