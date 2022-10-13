2c621929a7bd5a2921ab83dac53807d2dfabcd85
cornel <ghita.cornel@gmail.com>
2022-10-13 07:40:39 +0300

    better

--- elasticsearch/src/main/java/beans/config/IndexConfig.java
+++ elasticsearch/src/main/java/beans/config/IndexConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
@@ -12,6 +13,7 @@ import javax.persistence.EntityManager;
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class IndexConfig {
 
     private final EntityManager em;
@@ -19,8 +21,8 @@ public class IndexConfig {
     @PostConstruct
     public void initIndex() throws InterruptedException {
         log.info("Initiating indexing...");
-        SearchSession session = Search.session(em);
-        session.massIndexer().start();
+//        SearchSession session = Search.session(em);
+//        session.massIndexer().start();
         log.info("All entities indexed");
     }
 }
