205dad7de23cdf57c980f644229060090b7a2e8d
cornel <ghita.cornel@gmail.com>
2022-10-20 08:10:45 +0300

    better

--- rest-clients/src/test/java/beans/clients/feign/ExternalRestClientFeignTest.java
+++ rest-clients/src/test/java/beans/clients/feign/ExternalRestClientFeignTest.java
@@ -11,12 +11,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @WireMockTest
-@AutoConfigureMockMvc
 public class ExternalRestClientFeignTest {
 
     @Autowired
