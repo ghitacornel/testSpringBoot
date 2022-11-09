b633ef2e92c1d0198999564809731abdb8aad866
cornel <ghita.cornel@gmail.com>
2022-11-09 15:44:55 +0200

    better

--- elasticsearch/src/main/java/beans/service/ElasticSearchService.java
+++ elasticsearch/src/main/java/beans/service/ElasticSearchService.java
@@ -6,6 +6,7 @@ import beans.model.SimpleDataModel;
 import beans.projections.ChildProjection;
 import beans.projections.ParentProjection;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.search.engine.search.predicate.SearchPredicate;
 import org.hibernate.search.engine.search.predicate.dsl.BooleanPredicateClausesStep;
 import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
 import org.hibernate.search.mapper.orm.Search;
@@ -65,18 +66,18 @@ public class ElasticSearchService {
         SearchScope<Parent> scope = searchSession.scope(Parent.class);
         SearchPredicateFactory predicateFactory = scope.predicate();
         BooleanPredicateClausesStep<?> booleanJunction = predicateFactory.bool();
-        booleanJunction.must(predicateFactory.wildcard().field("content").matching("*" + content + "*"));// 3 matches
+        booleanJunction.must(predicateFactory.wildcard().field("content").matching("*" + content + "*"));// parents with id  in { 1 2 3 } are matched
 
-//        BooleanPredicateClausesStep<?> innerNestedBooleanPredicate = predicateFactory.bool();
-//        innerNestedBooleanPredicate.must(predicateFactory.wildcard().field("children.name").matching("john"));
-//        innerNestedBooleanPredicate.must(predicateFactory.wildcard().field("children.content").matching("first"));
-//
-//        SearchPredicate nestedPredicate = predicateFactory
-//                .nested()
-//                .objectField("children")
-//                .nest(innerNestedBooleanPredicate)
-//                .toPredicate();
-//        booleanJunction.must(nestedPredicate);
+        BooleanPredicateClausesStep<?> innerNestedBooleanPredicate = predicateFactory.bool();
+        innerNestedBooleanPredicate.must(predicateFactory.wildcard().field("children.name").matching("john"));
+        innerNestedBooleanPredicate.must(predicateFactory.wildcard().field("children.content").matching("first"));
+
+        SearchPredicate nestedPredicate = predicateFactory
+                .nested()
+                .objectField("children")
+                .nest(innerNestedBooleanPredicate)
+                .toPredicate();
+        booleanJunction.must(nestedPredicate);// only parent with child john+first is matched
 
         return searchSession.search(scope)
                 .select(f -> f.composite(ParentProjection::new,
