8717b3a0c54d0e146b7b7de9cb64099dd9a91640
cornel <ghita.cornel@gmail.com>
2022-11-19 11:23:03 +0200

    better

--- exceptions-validations/src/main/java/beans/validations/model/ValidationModel.java
+++ exceptions-validations/src/main/java/beans/validations/model/ValidationModel.java
@@ -4,10 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 @Entity
@@ -19,7 +16,7 @@ public class ValidationModel {
     private Integer id;
 
     @Size(min = 2, max = 30)
-    @NotEmpty
+    @NotBlank
     private String name;
 
 }
