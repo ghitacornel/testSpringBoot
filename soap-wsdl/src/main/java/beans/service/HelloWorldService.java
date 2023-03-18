e0753edb7b570cc640e9529f57aab6bc99b919b1
ghita <ghita.cornel@gmail.com>
2023-03-18 22:42:42 +0200

    soap server added

--- soap-wsdl/src/main/java/beans/service/HelloWorldService.java
+++ soap-wsdl/src/main/java/beans/service/HelloWorldService.java
@@ -1,12 +1,27 @@
 package beans.service;
 
-import com.tutorialspoint.helloworld.HelloWorldPortType;
+import com.blog.samples.webservices.accountservice.Account;
+import com.blog.samples.webservices.accountservice.AccountDetailsRequest;
+import com.blog.samples.webservices.accountservice.AccountDetailsResponse;
+import com.blog.samples.webservices.accountservice.EnumAccountStatus;
+import com.briansjavablog.accounts.Accounts;
 import org.springframework.stereotype.Service;
 
 @Service
-public class HelloWorldService implements HelloWorldPortType {
+public class HelloWorldService implements Accounts {
+
     @Override
-    public String greetings(String arg0) {
-        return "hello" + arg0;
+    public AccountDetailsResponse getAccountDetails(AccountDetailsRequest parameters) {
+
+        Account account = new Account();
+        account.setAccountNumber(parameters.getAccountNumber());
+        account.setAccountBalance(123);
+        account.setAccountName("custom account name");
+        account.setAccountStatus(EnumAccountStatus.ACTIVE);
+
+        AccountDetailsResponse response = new AccountDetailsResponse();
+        response.setAccountDetails(account);
+        return response;
+
     }
 }
