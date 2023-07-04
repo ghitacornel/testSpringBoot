02b29e36d900d39b488273067ddb69c23162fe70
ghita <ghita.cornel@gmail.com>
2023-07-04 17:43:08 +0300

    better

--- transactional/src/main/java/beans/transactional/service/LongRunningService.java
+++ transactional/src/main/java/beans/transactional/service/LongRunningService.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 @Slf4j
 public class LongRunningService {
