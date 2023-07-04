b5cb36f43b3d964581667d181625c51c61086324
ghita <ghita.cornel@gmail.com>
2023-07-04 17:07:50 +0300

    better

--- transactional/src/main/java/beans/TransactionalMain.java
+++ transactional/src/main/java/beans/TransactionalMain.java
@@ -10,8 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
-public class SpringBootMain {
+public class TransactionalMain {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMain.class, args);
+        SpringApplication.run(TransactionalMain.class, args);
     }
 }
