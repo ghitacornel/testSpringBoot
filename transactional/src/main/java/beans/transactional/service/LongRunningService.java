814bf3e103ea9c3090371a6c5e4ec11a7690cb61
ghita <ghita.cornel@gmail.com>
2023-07-04 17:16:35 +0300

    better

--- transactional/src/main/java/beans/transactional/service/LongRunningService.java
+++ transactional/src/main/java/beans/transactional/service/LongRunningService.java
@@ -3,6 +3,8 @@ package beans.transactional.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.*;
+
 @Service
 @Slf4j
 public class LongRunningService {
@@ -11,15 +13,30 @@ public class LongRunningService {
 
         log.info("start long running business");
 
-        // ensure the request takes at least a couple of seconds to complete
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Future<String> future = executorService.submit(() -> {
+
+            // ensure the request takes at least a couple of seconds to complete
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            return "OK";
+
+        });
+
+        executorService.shutdown();
+
+
         try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            String result = future.get();
+            log.info("end long running business");
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException("muie", e);
         }
 
-        log.info("end long running business");
-
-        return "OK";
     }
 }
