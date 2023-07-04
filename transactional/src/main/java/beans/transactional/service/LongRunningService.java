cfd992e106976477098a7f3790f93f195faf6d35
ghita <ghita.cornel@gmail.com>
2023-07-04 18:18:55 +0300

    better

--- transactional/src/main/java/beans/transactional/service/LongRunningService.java
+++ transactional/src/main/java/beans/transactional/service/LongRunningService.java
@@ -55,7 +55,9 @@ public class LongRunningService {
             // ensure the request takes at least a couple of seconds to complete
             try {
                 log.info("processing thread start" + Thread.currentThread());
-                this.wait(seconds * 1000);
+                synchronized (this) {
+                    this.wait(seconds * 1000);
+                }
                 log.info("processing thread stop" + Thread.currentThread());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
