bc549e1bf4a0143ad057675f869c0066d74b29ba
ghita <ghita.cornel@gmail.com>
2023-07-06 21:09:24 +0300

    better

--- aop/src/main/java/beans/aop/LogExecutionTime.java
+++ aop/src/main/java/beans/aop/LogExecutionTime.java
@@ -8,4 +8,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogExecutionTime {
+
+    // log as warning if specified timeout is exceeded
+    int warnTimeout() default 500;
+
 }
--- aop/src/main/java/beans/service/AOPAffectedService.java
+++ aop/src/main/java/beans/service/AOPAffectedService.java
@@ -10,8 +10,16 @@ public class AOPAffectedService {
         return "method1";
     }
 
-    @LogExecutionTime
+    @LogExecutionTime(timeout = 750)
     public String method2() {
+
+        // ensure execution time exceeds warning threshold
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         return "method2";
     }
 }
