7f38d0352efedf1ba0421c891493b7a54d8bb728
ghita <ghita.cornel@gmail.com>
2024-02-27 15:39:52 +0200

    better

--- envers/src/main/java/beans/SpringBootEnversMain.java
+++ envers/src/main/java/beans/SpringBootEnversMain.java
@@ -2,11 +2,8 @@ package beans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class SpringBootEnversMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootEnversMain.class, args);
