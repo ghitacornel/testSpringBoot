0917bb0854d2a2911ad75f0a86fc6c36da09fcc9
ghita <ghita.cornel@gmail.com>
2024-03-03 16:17:00 +0200

    better

--- docker/src/main/java/docker/model/CreatePersonDto.java
+++ docker/src/main/java/docker/model/CreatePersonDto.java
@@ -18,7 +18,7 @@ public class CreatePersonDto {
     @Schema(description = "name of person", example = "John", requiredMode = Schema.RequiredMode.REQUIRED)
     private String name;
 
-    @Schema(description = "date of birth of person", example = "2024-03-04", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(description = "date of birth of person", defaultValue = "2024-03-04", example = "2024-03-04", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime dateOfBirth;
 
