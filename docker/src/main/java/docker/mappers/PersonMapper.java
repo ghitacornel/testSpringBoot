648a1f065e0b09c86e3f57f7a38385c5a022f190
ghita <ghita.cornel@gmail.com>
2024-03-03 13:36:32 +0200

    better

--- docker/src/main/java/docker/mappers/PersonMapper.java
+++ docker/src/main/java/docker/mappers/PersonMapper.java
@@ -4,7 +4,6 @@ import docker.model.CreatePersonDto;
 import docker.model.PersonDto;
 import docker.model.entities.Person;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -15,7 +14,6 @@ public interface PersonMapper {
 
     PersonDto map(Person person);
 
-    @Mapping(target = "id", ignore = true)
     Person map(PersonDto personDto);
 
     List<PersonDto> map(List<Person> all);
