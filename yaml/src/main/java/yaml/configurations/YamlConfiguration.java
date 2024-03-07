a848bd08cc51a2b8d1f12330e89f308643b111c8
ghita <ghita.cornel@gmail.com>
2024-03-07 20:55:10 +0200

    better

--- yaml/src/main/java/yaml/configurations/YamlConfiguration.java
+++ yaml/src/main/java/yaml/configurations/YamlConfiguration.java
@@ -1,7 +1,6 @@
 package yaml.configurations;
 
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -9,7 +8,6 @@ import org.springframework.core.io.ClassPathResource;
 
 
 @Configuration
-@EnableConfigurationProperties
 public class YamlConfiguration {
 
     @Bean
