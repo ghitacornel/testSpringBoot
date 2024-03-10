e3d840051502a854f02448beb7be504ed667eabc
ghita <ghita.cornel@gmail.com>
2024-03-10 13:45:16 +0200

    better

--- docker/src/main/java/docker/model/entities/Person.java
+++ docker/src/main/java/docker/model/entities/Person.java
@@ -5,7 +5,6 @@ import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
