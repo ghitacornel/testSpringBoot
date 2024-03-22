9088a31b4155a8601ad0f069ca07941a1a6d24f1
ghita <ghita.cornel@gmail.com>
2024-03-22 17:17:57 +0200

    better

---
+++ jms/src/main/java/beans/jms/config/ActiveMQConfig.java
@@ -0,0 +1,30 @@
+package beans.jms.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ActiveMQConfig {
+
+    @Value("${spring.activemq.broker-url}")
+    private String BROKER_URL;
+
+    @Value("${spring.activemq.user}")
+    private String BROKER_USERNAME;
+
+    @Value("${spring.activemq.password}")
+    private String BROKER_PASSWORD;
+
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setTrustAllPackages(true);
+        connectionFactory.setBrokerURL(BROKER_URL);
+        connectionFactory.setPassword(BROKER_USERNAME);
+        connectionFactory.setUserName(BROKER_PASSWORD);
+        return connectionFactory;
+    }
+
+}
