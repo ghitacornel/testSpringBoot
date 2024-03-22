b9efa3182272f8dd3723ca724f85a27ff9f60ffd
ghita <ghita.cornel@gmail.com>
2024-03-22 16:34:25 +0200

    better

--- soap-wsdl/src/test/java/beans/WsdlCallTest.java
+++ soap-wsdl/src/test/java/beans/WsdlCallTest.java
@@ -1,11 +1,9 @@
 package beans;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,24 +11,20 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class WsdlCallTest {
-
-    @LocalServerPort
-    int port;
 
     @Autowired
     MockMvc mvc;
 
     @Test
-    @Disabled("FUCK SOAP")
     public void testWsdlCall() throws Exception {
 
         String request = Utils.readFile("WsdlRequest.xml");
         String response = Utils.readFile("WsdlResponse.xml");
 
-        mvc.perform(post("http://localhost:" + port + "/services/Hello/")
+        mvc.perform(post("/services/Hello/")
                         .contentType(MediaType.APPLICATION_XML)
                         .content(request))
                 .andExpect(status().isOk())
