d0babee37baa32008b49dd5a69185de7cd5acc74
ghita <ghita.cornel@gmail.com>
2024-03-23 11:16:43 +0200

    better

--- open-api/src/main/java/beans/SwaggerMain.java
+++ open-api/src/main/java/beans/SwaggerMain.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMain {
+public class SwaggerMain {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMain.class, args);
+        SpringApplication.run(SwaggerMain.class, args);
     }
 }
