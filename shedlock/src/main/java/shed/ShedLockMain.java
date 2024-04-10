cd1b72ac8a81d2b8d2796f88268c878a91f34bce
ghita <ghita.cornel@gmail.com>
2024-04-10 22:26:36 +0300

    better

---
+++ shedlock/src/main/java/shed/ShedLockMain.java
@@ -0,0 +1,11 @@
+package shed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedLockMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ShedLockMain.class, args);
+    }
+}
---
+++ shedlock/src/main/java/shed/timer/TimerComponent.java
@@ -0,0 +1,20 @@
+package shed.timer;
+
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class TimerComponent {
+
+    // task is scheduled to run every 6 seconds
+    @Scheduled(cron = "0/6 * * * * *")
+    // lock prevents the previous scheduling rule, it keeps the lock for at least 10 seconds and at most 15 seconds
+    @SchedulerLock(name = "scheduledTaskName", lockAtLeastFor = "PT10S", lockAtMostFor = "PT15S")
+    public void scheduledTaskEvery6Seconds() {
+        log.info("scheduled task executed by " + Thread.currentThread());
+    }
+
+}
