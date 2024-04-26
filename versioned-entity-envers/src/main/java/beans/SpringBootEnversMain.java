25164b439cd4c4d51efb6f4b7309a626e3cd1e9e
ghita <ghita.cornel@gmail.com>
2024-04-26 14:37:21 +0300

    better

--- versioned-entity-envers/src/main/java/beans/SpringBootEnversMain.java
+++ versioned-entity-envers/src/main/java/beans/SpringBootEnversMain.java
@@ -2,9 +2,7 @@ package beans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootEnversMain {
     public static void main(String[] args) {
