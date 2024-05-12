71514a8ba13da2de1b6663c8d723ab253895b190
ghita <ghita.cornel@gmail.com>
2024-05-12 22:37:03 +0300

    better

---
+++ logging/src/main/java/beans/scheduler/info/InfoLogger.java
@@ -0,0 +1,24 @@
+package beans.scheduler.info;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+class InfoLogger {
+
+    // log all types every second
+    @Scheduled(cron = "0/1 * * * * *")
+    public void log() {
+
+        // log all types
+        log.info("info message {}", this);
+        log.warn("warn message {}", this);
+        log.error("error message {}", this);
+        log.debug("debug message {}", this);
+        log.trace("trace message {}", this);
+
+    }
+
+}
