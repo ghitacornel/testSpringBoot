e704f81f5a7873cba106dd548e2ae977cb250011
cornel <ghita.cornel@gmail.com>
2024-11-21 16:48:23 +0200

    solr

--- elasticsearch/src/main/java/beans/config/DataSetup.java
+++ elasticsearch/src/main/java/beans/config/DataSetup.java
@@ -6,13 +6,12 @@ import beans.model.SimpleDataModel;
 import beans.model.Status;
 import beans.repository.ParentRepository;
 import beans.repository.SimpleDataModelRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 @RequiredArgsConstructor
