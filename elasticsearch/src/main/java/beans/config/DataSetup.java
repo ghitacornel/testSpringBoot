c4cdca91fe34677f36851554b484d932fcc29843
cornel <ghita.cornel@gmail.com>
2024-11-21 17:23:42 +0200

    elk

--- elasticsearch/src/main/java/beans/config/DataSetup.java
+++ elasticsearch/src/main/java/beans/config/DataSetup.java
@@ -8,14 +8,12 @@ import beans.repository.ParentRepository;
 import beans.repository.SimpleDataModelRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
-@Slf4j
 public class DataSetup {
 
     private final ParentRepository parentRepository;
