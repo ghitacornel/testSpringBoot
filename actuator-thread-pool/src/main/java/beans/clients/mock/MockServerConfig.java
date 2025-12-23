a99825744994634ee2b09fc4d6cd747b3eab6db3
cornel <ghita.cornel@gmail.com>
2025-12-23 17:03:09 +0200

    better

--- actuator-thread-pool/src/main/java/beans/clients/mock/MockServerConfig.java
+++ actuator-thread-pool/src/main/java/beans/clients/mock/MockServerConfig.java
@@ -4,6 +4,7 @@ import beans.clients.model.ClientResponseDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -18,13 +19,22 @@ class MockServerConfig {
     @Autowired
     protected ObjectMapper objectMapper;
 
+    private WireMockServer wireMockServer;
+
     @PostConstruct
     @SneakyThrows
     void init() {
-        WireMockServer wireMockServer = new WireMockServer(1111);
+        wireMockServer = new WireMockServer(1111);
         wireMockServer.start();
+
         wireMockServer.stubFor(get("/externalService/ok").willReturn(okJson(objectMapper.writeValueAsString(new ClientResponseDto("ok GET dummy content from 3rd pary client" + Thread.currentThread())))));
         wireMockServer.stubFor(get("/externalService/delay").willReturn(okJson(objectMapper.writeValueAsString(new ClientResponseDto("ok GET dummy content with delay from 3rd pary client" + Thread.currentThread())))
                 .withFixedDelay(10000)));
+    }
+
+    @PreDestroy
+    @SneakyThrows
+    void destroy() {
+        wireMockServer.stop();
     }
 }
