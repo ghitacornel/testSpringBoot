d64273f525feb41f07630de90fad392efded0923
cornel <ghita.cornel@gmail.com>
2025-12-23 17:04:08 +0200

    better

--- actuator-thread-pool/src/main/java/beans/clients/mock/MockServerConfig.java
+++ actuator-thread-pool/src/main/java/beans/clients/mock/MockServerConfig.java
@@ -7,7 +7,6 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
