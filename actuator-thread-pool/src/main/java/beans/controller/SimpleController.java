fa54e7463c1168dca5056b2e6dc96185f7e5f81e
cornel <ghita.cornel@gmail.com>
2025-12-23 16:38:13 +0200

    better

--- actuator-thread-pool/src/main/java/beans/controller/SimpleController.java
+++ actuator-thread-pool/src/main/java/beans/controller/SimpleController.java
@@ -1,9 +1,11 @@
 package beans.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "simple")
 class SimpleController {
@@ -11,6 +13,23 @@ class SimpleController {
     @GetMapping
     public String testSimple() {
         return "all good";
+    }
+
+    @GetMapping("delay")
+    public String testSimpleWithLongDelay() {
+
+        log.info("delaying thread {}", Thread.currentThread().getName());
+
+        //just sleep
+        try {
+            Thread.currentThread().sleep(30000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        log.info("delayed thread {}", Thread.currentThread().getName());
+
+        return "all good with delay for thread " + Thread.currentThread().getName();
     }
 
 }
